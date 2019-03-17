a854ab06aa7a0e4d41353a57f3d5ce0a11df9123
Pavel Shanhin <shanMail@tut.by>
2019-03-17 20:07:47 +0300

    feat: task solving

--- src/index.js
+++ src/index.js
@@ -4,4 +4,17 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+  let amountOfTriangle = 0;
+
+ for (let index = 0; index < preferences.length; index++) {
+  const connect1 = preferences[index] - 1;
+  const connect2 = preferences[connect1] - 1;
+  const connect3 = preferences[connect2] - 1;
+
+  if (index === connect3) {
+    amountOfTriangle += 1;
+  }
+ }
+
+  return Math.floor(amountOfTriangle / 3);
 };
